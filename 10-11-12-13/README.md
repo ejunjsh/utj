8018de0c8d064410a56c5da0876e06ed747cf49f
zhouff <sjj050121014@163.com>
2018-09-04 20:17:17 +0800

    add docker to 10-11-12-13

--- 10-11-12-13/README.md
+++
@@ -1,3 +0,0 @@
-# test result
-
-don't provide the docker environment to test,you can run in your host.
--- 10-11-12-13/run.sh
+++ 10-11-12-13/run.sh
@@ -1,2 +1,2 @@
-javac -encoding utf8 NameCheckProcessor.java NameCheck.java
+javac -encoding utf8 NameCheckProcessor.java NameChecker.java
 javac -encoding utf8 -processor NameCheckProcessor BADLY_NAMED_CODE.java
