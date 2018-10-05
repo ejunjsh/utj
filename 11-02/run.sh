9120ebbf34d5d215256663f537d774eab6224ab0
zhouff <sjj050121014@163.com>
2018-10-05 13:09:35 +0800

    11-02

--- 11-02/run.sh
+++ 11-02/run.sh
@@ -1,1 +1,5 @@
-javac -encoding utf8 test.java&&java -XX:+PrintCompilation -XX:+PrintInlining test&&java -XX:+PrintOptoAssembly test
+javac -encoding utf8 test.java
+echo "========below shows which code is compiled and inlined========"
+java -XX:+PrintCompilation -XX:+PrintInlining test
+echo "========below shows the assembly========"
+java -XX:+PrintOptoAssembly test
